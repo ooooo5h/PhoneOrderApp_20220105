507b972ec0e6f83b31dcf3ad8bd12a2c32d08069
ooooo5h <ooooo5h91@gmail.com>
2022-01-05 17:09:26 +0900

    TedPermission으로 권한 획득해서 추가적으로 ACTION-call 기능 활용(전화 연결)

--- app/src/main/java/com/neppplus/phoneorderapp_20220105/ViewStoreDetailActivity.kt
+++ app/src/main/java/com/neppplus/phoneorderapp_20220105/ViewStoreDetailActivity.kt
@@ -1,8 +1,14 @@
 package com.neppplus.phoneorderapp_20220105
 
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.bumptech.glide.Glide
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.normal.TedPermission
 import com.neppplus.phoneorderapp_20220105.models.StoreData
 import kotlinx.android.synthetic.main.activity_view_store_detail.*
 
@@ -16,5 +22,31 @@ class ViewStoreDetailActivity : AppCompatActivity() {
         txtStoreName.text = storeData.name
         txtPhoneNum.text = storeData.phoneNum
         Glide.with(this).load(storeData.logoURL).into(imgLogo)
+
+        btnCall.setOnClickListener {
+
+            val pl = object : PermissionListener {
+                override fun onPermissionGranted() {
+
+                    val myUri = Uri.parse("tel:${storeData.phoneNum}")
+                    val myIntent = Intent(Intent.ACTION_CALL, myUri)
+                    startActivity(myIntent)
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+
+                    Toast.makeText(this@ViewStoreDetailActivity, "권한이 거절되어 전화연결이 불가능합니다.", Toast.LENGTH_SHORT).show()
+                }
+
+
+            }
+
+            TedPermission.create()
+                .setPermissionListener(pl)
+                .setPermissions((Manifest.permission.CALL_PHONE))
+                    .check()
+
+
+        }
     }
 }
